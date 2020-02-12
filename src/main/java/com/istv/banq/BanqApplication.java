0aa76a80caab715e16be304810b8f10c2afdd850
fdecouv <decouvelaere.florian@gmx.fr>
2020-02-12 19:11:27 +0100

    add spring initializr project

---
+++ src/main/java/com/istv/banq/BanqApplication.java
@@ -0,0 +1,13 @@
+package com.istv.banq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/istv/banq/BanqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istv.banq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
