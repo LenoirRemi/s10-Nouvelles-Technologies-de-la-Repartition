d67c98a61fae724813b84a5357db3a8d3b5ba4a1
fdecouv <decouvelaere.florian@gmx.fr>
2020-04-06 14:11:48 +0200

    add mainController

---
+++ src/main/java/com/istv/banq/controller/MainController.java
@@ -0,0 +1,33 @@
+package com.istv.banq.controller;
+
+import com.istv.banq.model.User;
+import com.istv.banq.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+public class MainController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value="/home", method = RequestMethod.GET)
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName());
+        modelAndView.setViewName("home.html");
+        return modelAndView;
+    }
+}
